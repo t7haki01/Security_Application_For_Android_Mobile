d9b7be8a5fa18b04f2454b77fc5ee0f652fb36dc
Sandro K <kallingers@gmail.com>
2019-03-26 10:10:31 +0200

    Design of navigation bar (menu) finished

--- app/src/main/java/scurity/app/securityapplicationforandroidmobile/MainActivity.java
+++ app/src/main/java/scurity/app/securityapplicationforandroidmobile/MainActivity.java
@@ -1,10 +1,8 @@
 package scurity.app.securityapplicationforandroidmobile;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +12,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity
@@ -85,17 +81,15 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_wifiscanner) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_settingschecker) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_usagetracker) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_theftalarm) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_aboutapp) {
 
         }
 
