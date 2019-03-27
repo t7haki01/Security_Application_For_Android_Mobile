fbca7f941d3efe07422e4a3fe83de6f5f9a911d4
t7haki01 <t7haki01@students.oamk.fi>
2019-03-27 14:43:37 +0200

    rounded button in fragment

--- app/src/main/java/scurity/app/securityapplicationforandroidmobile/WifiScannerFragment.java
+++ app/src/main/java/scurity/app/securityapplicationforandroidmobile/WifiScannerFragment.java
@@ -1,11 +1,17 @@
 package scurity.app.securityapplicationforandroidmobile;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
 /**
@@ -26,4 +32,14 @@ public class WifiScannerFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_wifi_scanner, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Context context = getContext();
+        FrameLayout wifiFrame = getView().findViewById(R.id.frame_wifiScanner);
+
+        Button wifiBtn = new Button(context);
+
+//        wifiBtn.setBott
+
+    }
 }
