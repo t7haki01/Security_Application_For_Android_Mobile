3b84d364a2320379bb8c85a2b91419aa18d82267
FranciG <beodo0@gmail.com>
2019-04-08 14:06:00 +0300

    Changes to be committed: Created fragment for basic info

--- app/src/main/java/scurity/app/securityapplicationforandroidmobile/basic_info.java
+++ app/src/main/java/scurity/app/securityapplicationforandroidmobile/basic_info.java
@@ -1,11 +1,16 @@
 package scurity.app.securityapplicationforandroidmobile;
 
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -27,4 +32,38 @@ public class basic_info extends Fragment {
         return inflater.inflate(R.layout.fragment_basic_info, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getActivity().getWindowManager()
+        .getDefaultDisplay().getMetrics(metrics);
+
+
+        float height=metrics.heightPixels/metrics.xdpi;
+        float width=metrics.widthPixels/metrics.ydpi;
+//To do. It is needed to add the fragment to somewhere now
+        TextView textView =  getView().findViewById(R.id.basic);
+
+        textView.setText(
+                "The screen size is:"+(float)Math.sqrt(height*height+width*width) +"\n" +
+                        "Manufacturer: " + Build.MANUFACTURER +"\n"
+
+                        +  "Brand:" + Build.BRAND+"\n"
+                        +"Model: " + Build.MODEL+"\n"
+                        +  "Board: " + Build.BOARD+"\n"
+                        + "Hardware: " + Build.HARDWARE+"\n"
+                        +  "Serial: " + Build.SERIAL+"\n"
+
+                        + "Bootloader: " + Build.BOOTLOADER+"\n"
+                        + "User: " + Build.USER+"\n"
+                        +  "Host: " + Build.HOST+"\n"
+                        + "Android version: " + Build.VERSION.RELEASE+"\n"
+                        +  "API level: " + Build.VERSION.SDK_INT + ""+"\n"
+                        +  "Build ID: " + Build.ID+"\n"
+                        +  "Build time: " + Build.TIME +"\n"
+                        +  "Fingerprint: " + Build.FINGERPRINT+"\n"
+
+        );
+
+    }
 }
