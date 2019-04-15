e5875f58cae03e8e3f2dd1a8018a84e9f1ac4a7d
FranciG <beodo0@gmail.com>
2019-04-15 13:50:10 +0300

    Emergency call fragment implemented

--- app/src/main/java/scurity/app/securityapplicationforandroidmobile/EmergencyFragment.java
+++ app/src/main/java/scurity/app/securityapplicationforandroidmobile/EmergencyFragment.java
@@ -1,17 +1,30 @@
 package scurity.app.securityapplicationforandroidmobile;
 
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EmergencyFragment extends Fragment {
+public class EmergencyFragment extends Fragment implements View.OnClickListener {
+    private static final String[] CALL_PERMISSIONS = {
+            Manifest.permission.CALL_PHONE
+    };
+
+    private Button dial;
+    
 
 
     public EmergencyFragment() {
@@ -22,7 +35,36 @@ public class EmergencyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_emergency, container, false);
+        View view= inflater.inflate(R.layout.fragment_emergency, container, false);
+        Context context=view.getContext();
+//paste
+        dial = (Button) view.findViewById(R.id.btn_dial);
+        dial.setOnClickListener(this);
+        int permissionCallPhone = ActivityCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE);
+        if (permissionCallPhone != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    getActivity(),
+                    CALL_PERMISSIONS,
+                    1
+
+            );
+        }
+
+    return view;
+
     }
 
+
+    //paste the rest
+
+    public void onDialButton (View v){
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:+123"));
+        startActivity(intent);
+        //verifyPermissions();
+    }
 }
